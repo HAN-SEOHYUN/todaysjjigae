f6d107b9bb81dd80ed2b7afedaca49e58db5d2b5
HAN-SEOHYUN <000312@daum.net>
2022-11-17 15:13:30 +0900

    Initial commit

---
+++ src/main/kotlin/com/proj/todaysjjigae/TodaysjjigaeApplication.kt
@@ -0,0 +1,11 @@
+package com.proj.todaysjjigae
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodaysjjigaeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodaysjjigaeApplication>(*args)
+}
