c4bd58f8364490bc9ad9c1b9c317d439de2edbf7
HAN-SEOHYUN <000312@daum.net>
2022-11-19 17:30:04 +0900

    feat : Add custom Exception

---
+++ src/main/kotlin/com/proj/todaysjjigae/utils/exceptions/JjigaeException.kt
@@ -0,0 +1,4 @@
+package com.proj.todaysjjigae.utils.exceptions
+
+class JjigaeException(override val message: String?): Exception(message) {
+}
--- src/main/kotlin/com/proj/todaysjjigae/web/rest/JjigaeResource.kt
+++ src/main/kotlin/com/proj/todaysjjigae/web/rest/JjigaeResource.kt
@@ -2,6 +2,9 @@ package com.proj.todaysjjigae.web.rest
 
 import com.proj.todaysjjigae.dto.JjigaeDTO
 import com.proj.todaysjjigae.service.JjigaeService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 class JjigaeResource(
     private val jjigaeService: JjigaeService
 ) {
+
     @PostMapping
     fun createJjigae(@RequestBody jjigaeDTO: JjigaeDTO) : JjigaeDTO{
         return jjigaeService.createJjigae(jjigaeDTO)
